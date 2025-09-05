e682ac8a7254246c6570cb434c7cc45e54b1f553
kim <a01064149895@gmail.com>
2025-09-05 17:02:32 +0900

    2025-09-05 js commit!

--- cgv-demo/support/support.js
+++ cgv-demo/support/support.js
@@ -1,25 +1,43 @@
 //DOM 트리 생성(body 실행)후 DOMContentLoaded 함수 처리
 window.addEventListener('DOMContentLoaded', function() {
-    createTable();
+    createTable('all');
     filterMenuEvent();
 })//Window.addEventListener
 
-function filterMenuEvent() {
+function menuDefaultColor(menu) {
+    menu.style.background = 'rgb(137, 137, 135)';
+    menu.style.borderLeft = `1px solid var(rgb(137, 137, 135))`;
+    menu.style.borderBottom = `2px solid var(rgb(137, 137, 135))`;
+}
+
+function menuSelectColor(menu) {
+    menu.style.background = 'rgb(251, 67, 87)';
+    menu.style.borderLeft = `1px solid var(rgb(251, 67, 87))`;
+    menu.style.borderBottom = `2px solid var(rgb(251, 67, 87))`;
+}
+
+function filterMenuEvent(type) {
     let menuList = document.querySelectorAll(".filter-menu a");
+
+    //처음 호출 시 버튼 색상
+    menuList.forEach(menu => {  //처음 호출 시 버튼 색상
+        menu.id === 'all'?menuSelectColor(menu) : menuDefaultColor(menu);
+    });
+    
     menuList.forEach((menu) => {
         menu.addEventListener('click', () => {
-            let type = menu.id;
-            filterData(type);
+            menuList.forEach(menu => menuDefaultColor(menu));
+            menuSelectColor(menu);
+            createTable(menu.id);
         });
-    })
+    });
 }
 
 async function filterData(type) {
     let list = await getData();
     let filterList = list.filter((item) => item.type === type);
-    console.log(filterList);
-    createTable(filterList);
-    
+    // console.log(filterList);
+    return filterList;
 }
 
 async function getData() {
@@ -27,8 +45,13 @@ async function getData() {
     return response.json();
 }
 
-async function createTable(list) {  //list는 array type
-    // let list = await getData();
+async function createTable(type) {  //list는 array type
+    let list = null;
+    if(type === 'all'){
+        list = await getData();
+    } else {
+        list = await filterData(type);
+    }
 
     let output = `
         <table class="stable">
