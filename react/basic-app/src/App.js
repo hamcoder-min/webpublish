b68f078687bb54019859bcce11af589454b49e3b
kim <a01064149895@gmail.com>
2025-09-08 12:44:48 +0900

    2025-09-08 react commit!

---
+++ react/basic-app/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+      <h1 className="App">Hello~ React App!!!</h1>
+    </div>
+  );
+}
+
+export default App;
