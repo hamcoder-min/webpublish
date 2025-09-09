1e670721f197f6db361e1cb868b7b9a11088e652
kim <a01064149895@gmail.com>
2025-09-09 16:01:21 +0900

    2025-09-09 React commit!

--- react/basic-app/src/component/MyButton.jsx
+++ react/basic-app/src/component/MyButton.jsx
@@ -1,6 +1,14 @@
 
-export function MyButton({name, type, style}) {  //props = {name:"회원가입"}
+export function MyButton({name, type, style, handleProps}) {  //props = {name:"회원가입"}
+    const handleClick = () => {
+        console.log(`(자식)버튼클릭! --------> ${name}`);
+        handleProps(`버튼클릭! --------> ${name}`);
+    }
+
     return (
-        <button type={type} class={style}>{name}</button>
+        <button type={type} 
+                class={style}
+                onClick={handleClick}
+                >{name}</button>
     );
 }
