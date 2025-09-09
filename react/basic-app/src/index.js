b42ac9c5ad7da120f49ed42c80114e398546dfd6
kim <a01064149895@gmail.com>
2025-09-09 16:21:24 +0900

    2025-09-09 React commit!

--- react/basic-app/src/index.js
+++ react/basic-app/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App.js';
 // import App from './AppAvartar.js';
-// import App from './AppButton.js';
-import App from './AppCounter.js';
+import App from './AppButton.js';
+// import App from './AppCounter.js';
 import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
