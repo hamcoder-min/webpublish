c4ab6cb2b5dcb7cb40b294ee1e5ef65ab746e75d
kim <a01064149895@gmail.com>
2025-09-10 16:24:34 +0900

    2025-09-10 React commit!

--- react/basic-app/src/component/Product.jsx
+++ react/basic-app/src/component/Product.jsx
@@ -1,20 +1,48 @@
+import { useState, useEffect } from "react";
+
 /**
  * 베스트 상품 컴포넌트
  */
 export function BestProduct() {
+    const [cartCount, setCartCount] = useState(0);
+    const [bestProductList, setBestProductList] = useState([]);
+
+    const handleCartCount = () => {
+        setCartCount(cartCount + 1);
+    }
+
+    //컴포넌트 호출 시 데이터 로딩(비동기 처리) --> useEffect Hooks 함수
+    useEffect(() => {
+        // fetch("/data/best_products.json")
+        //     .then(response => response.json())
+        //     .then(jsonData => setBestProductList(jsonData))
+        //     .catch(error => console.log(error));
+
+        const fetchData = async() => {
+            const response = await fetch("/data/best_products.json");
+            const jsonData = await response.json();
+            setBestProductList(jsonData);
+        }
+        fetchData();
+
+        }, []);    //[]안의 값이 바뀌면 콜백 함수 다시 실행
+
     return (
         <>
-            <h2>베스트 상품</h2>
+            <h2>베스트 상품 - 🛒({cartCount})</h2>
             <ul className="best-product">
-                <li><BestProductImage /></li>
-                <li><BestProductItem /></li>
-                <li><BestProductItem /></li>
-                <li><BestProductItem /></li>
-                <li><BestProductItem /></li>
-                <li><BestProductItem /></li>
-                <li><BestProductItem /></li>
-                <li><BestProductItem /></li>
-                <li><BestProductItem /></li>
+                {bestProductList.map((product, index) => 
+                    (index === 0) ? 
+                        <li><BestProductImage 
+                                img={product.img}
+                                style={{width: "600px", height: "800px"}} 
+                                rank={product.rank}
+                                like={product.like}
+                                cartCount={handleCartCount}/></li>
+                    : <li><BestProductItem 
+                                item={product}
+                                cartCount={handleCartCount}/></li>
+                )}
             </ul>
         </>
     );
@@ -23,11 +51,21 @@ export function BestProduct() {
 /**
  * 베스트 상품 아이템 컴포넌트
 */
-export function BestProductItem() {
+export function BestProductItem({item, cartCount}) {
+    
     return(
         <div>
-            <BestProductImage />
-            <BestProductContent />
+            <BestProductImage  
+                    img={item.img}
+                    style ={{width: "200px", height: "300px"}}
+                    rank={item.rank}
+                    like={item.like}
+                    cartCount={cartCount}/>
+            <BestProductContent 
+                    title={item.title}
+                    sale={item.sale}
+                    price={item.price}
+                    like={item.like} />
         </div>
     );
 }
@@ -35,13 +73,14 @@ export function BestProductItem() {
 /**
  * 베스트 상품 컨텐츠 컴포넌트
  */
-export function BestProductContent() {
+export function BestProductContent({title, sale, price, like}) {
     return(
         <div className="best-product-content">
-            <p className="best-product-content-title">HALF SLEEVE KNIT TOP OLIVE_UDSW5C201K1</p>
-            <span className="best-product-content-sale">30%</span>
-            <span className="best-product-content-price">21,500</span>
-            <span className="best-product-content-like">🤍</span>
+            <p className="best-product-content-title">{title}</p>
+            <span className="best-product-content-sale">{sale}</span>
+            <span className="best-product-content-price">{price}</span>
+            { like ? 
+                <span className="best-product-img-like">🤍</span> : "" }
         </div>
     );
 }
@@ -49,22 +88,17 @@ export function BestProductContent() {
 /**
  * 베스트 상품 이미지 컴포넌트
  */
-export function BestProductImage() {
-    const data = {
-    "img": "/images/product4.jpg",
-    "style": {
-        "width": "200px",
-        "height": "300px"
-    },
-    "no": 1,
-    "like": false
-}
+export function BestProductImage({img, style, rank, like, cartCount}) {
+    const handleAddCart = () => {
+        cartCount();
+    }
     return (
         <div className="best-product-img">
-            <ProductImage img={data.img} style={data.style} />
-            <span className="best-product-img-no">{data.no}</span>
-            { data.like ? 
-                <span className="best-product-img-like">🤍</span> : "" }
+            <ProductImage img={img} style={style} />
+            <span className="best-product-img-no">{rank}</span>
+            { like ? 
+                <span   className="best-product-img-like"
+                        onClick={handleAddCart}>🛒</span> : "" }
         </div>
     );
 }
