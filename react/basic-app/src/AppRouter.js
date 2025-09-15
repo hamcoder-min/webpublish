6890b25f47c5d3bd40ade9dbbb48ec5ca3595b1d
kim <a01064149895@gmail.com>
2025-09-15 16:39:25 +0900

    2025-09-15 React commit!

--- react/basic-app/src/AppRouter.js
+++ react/basic-app/src/AppRouter.js
@@ -1,7 +1,20 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Layout } from './components/router/Layout.jsx';
+import { Login } from './components/form/Login.jsx'
+import { Signup } from './components/form/Signup.jsx'
 
 export default function App() {
     return(
-        <></>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Layout/>}>
+                        <Route path='/login' element={<Login/>} />    //path와 to가 동일해야함(Lyaout 안에 있는)
+                        <Route path='/signup' element={<Signup/>} />    //path와 to가 동일해야함(Lyaout 안에 있는)
+                        
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
