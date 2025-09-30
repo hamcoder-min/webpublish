894097176a38e9d6c886044e0a0b7010662742b5
kim <a01064149895@gmail.com>
2025-09-30 11:02:52 +0900

    2025-09-30 React commit!

---
+++ react/shoppy-app-redux/src/hooks/useAuth.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext.js';
+
+/**
+ * AuthContext에 저장된 데이터를 제어하는 커스텀훅 함수
+ */
+export function useAuth() {
+    const {isLogin, setIsLogin} = useContext(AuthContext);
+
+    /** Login */
+    const handleLogin = (userId) => {
+        setIsLogin(!isLogin);
+        const loginInfo = {
+            "userId": userId,
+            "token": "wefsdlfj456465"
+        }
+        //로그인 정보를 브라우저 > Application Tab > Local Storage에 객체를 문자열로 저장
+        localStorage.setItem('loginInfo', JSON.stringify(loginInfo));
+    }
+
+    /** Logout */
+    const handleLogout = () => {
+        alert("로그아웃 되었습니다.");
+        setIsLogin(!isLogin);
+        localStorage.removeItem("loginInfo");
+    }
+
+    return {handleLogin, handleLogout}
+}
